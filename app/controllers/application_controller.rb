404d712e3bd88dc5a744edaaf93733ac6d58f045
PenPeen <ikegami1022.h@gmail.com>
2025-09-14 19:35:25 +0900

    ヘルスチェック長すぎ問題解消

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,27 +3,7 @@ class ApplicationController < ActionController::Base
     render json: {
       status: 'ok',
       version: ENV.fetch('DEPLOYMENT_VERSION', 'unknown'),
-      timestamp: Time.current.iso8601,
-      checks: {
-        database: database_check,
-        redis: redis_check
-      }
+      timestamp: Time.current.iso8601
     }
-  end
-
-  private
-
-  def database_check
-    ActiveRecord::Base.connection.execute('SELECT 1')
-    'ok'
-  rescue => e
-    'error'
-  end
-
-  def redis_check
-    Sidekiq.redis { |conn| conn.ping }
-    'ok'
-  rescue => e
-    'error'
   end
 end
